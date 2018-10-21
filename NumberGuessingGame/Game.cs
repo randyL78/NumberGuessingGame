dcccd4b339a79a04f6164c6de466dbcca23b2d8f
Randy's Mac <randydavidl78@gmail.com>
2018-10-20 20:43:09 -0400

    Add Game.cs and Scores.cs
    * Game.cs
     * houses main function
     * controls outer loop
     * only a stub currently
    * Score.cs
     * handles score board operations
     * only a stub currently

---
+++ NumberGuessingGame/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace NumberGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Round round = new Round();
+            
+            Console.WriteLine(round.CheckAnswer(14));
+            Console.WriteLine("You have made " + round.Guesses + " guess(es) so far");
+            if (round.HasWon)
+                Console.WriteLine("Congratulations, you won!");
+            
+
+
+        }
+    }
+}
---
+++ NumberGuessingGame/Scores.cs
@@ -0,0 +1,7 @@
+namespace NumberGuessingGame
+{
+    public class Scores
+    {
+        
+    }
+}
